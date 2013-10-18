522a3417847bd05ef97de60bc5af9198de4860f7
Apprentice <apprentice@dbc25.local>
2013-10-18 16:13:09 -0700

    This file has all the controller class filled in.  The Model and View classes still not filled in

--- flashcards.rb
+++ flashcards.rb
@@ -1,12 +1,12 @@
 # Main file for team copacetic's flashcards
 
 require 'csv'
-require_relative 'CSVModel'
+require_relative 'TextParser'
 require_relative 'Card'
 require_relative 'Deck'
 require_relative 'View'
 
-class CSVModel
+class TextParser
   #i: csv file 
   def initialize(csv_file)
     @file = csv_file
@@ -23,7 +23,7 @@ class CSVModel
   end
   #o: array of hashes
 
-  def run
+  def self.run
     # call load and arrays_to_hashes, return array of hashes
   end
 
@@ -94,40 +94,55 @@ end
 class Controller
 
   def initialize(file)
+    @file=file
     @game_view = View.new
     @game_deck = Deck.new
-    @game_csv = CSVModel.new(file)
   end
 
   #i:
   # conversion from array of hashes to created cards
-  def card_factory(array_of_hashes)
-    
+  def card_factory(unformatted_cards)
+    unformatted_cards.each do |hash|
+      add_cards_to_deck(Card.new(hash))
+    end 
   end
   #o: card objects as an array
 
   #i: the output from card factory
-  def add_cards_to_deck
-    
+  def add_cards_to_deck(card)
+    @game_deck.deck << card
   end
   #o:  push cards into the deck class
 
   def run
-    # calls @game_csv.run
-    # other stuff too; drives whole program
+    #possible loop
+    unformatted_cards = TextParser.run(@file)
+    card_factory(unformatted_cards)
+    @game_view.game_intro
+    @game_view.print_definition
+    user_input = get_user_input
+    evaluate_user_input(user_input)
   end
 
   def get_user_input
-    # calls @game_view.get_answer
-    # uses gets.chomp to get input
+    @game_view.get_answer
+    @answer=gets.chomp
     # returns "input" for evaluate_user_input
   end
 
   def evaluate_user_input(input)
     # determine whether user is guessing or giving command
-    # if guess call is_input_true? on downcase
+    if is_input_true?
+      @game_view.you_rock
+    else 
+      @game_view.try_again
+    end
     # else execute command
   end
+
+  def is_input_true?
+    @answer.downcase == get_top_card_answer.downcase
+  end  
 
   def get_top_card_answer
     @game_deck.get_top_card.answer
@@ -141,3 +156,10 @@ end
 filename = 'flashcard_samples.txt'
 our_controller = Controller.new(filename)
 our_controller.run
+
+# talk to viewer have viewer prompt
+# viewer provides a definition
+# get_user_input
+# evaluate_user_input
+# is_input-true?
+
